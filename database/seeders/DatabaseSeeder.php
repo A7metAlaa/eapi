4162721c62007cf296baee12624bbf9794e8db29
Ahmed Alaa <Ahmedx3la22@gmail.com>
2024-09-05 02:12:39 +0300

    created customer table in reviews & seeding data fro product and reviews in DB

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-    }
+        Product::factory(10)->create();
+        Review::factory(10)->create();
+     }
 }
